5b1e277b66873c3a48f8c881af06eeccb1da043a
PanyL <1481249965@qq.com>
2025-07-25 16:29:59 +0800

    项目9 GCD中央

--- Project9/Project9/DetailViewController.swift
+++ Project9/Project9/DetailViewController.swift
@@ -4,14 +4,37 @@
 //
 //  Created by Pyl on 2025/7/24.
 //
-
+import WebKit
 import UIKit
 
 class DetailViewController: UIViewController {
 
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        guard let detailItem = detailItem else { return }
+        let html = """
+            <html>
+            <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <style> body {font-size:150%;} </style>
+            </head>
+            <body>
+            \(detailItem.body)
+            </body>
+            </html>
+            """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+        
         // Do any additional setup after loading the view.
     }
     
@@ -26,4 +49,4 @@ class DetailViewController: UIViewController {
     }
     */
 
-}
+
--- Project9/Project9/ViewController.swift
+++ Project9/Project9/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    
+
     var petitions = [Petition]()  //原始数据
     var filteredPetitions = [Petition]()  //筛选后的数据
     @objc func showCredits() {
@@ -68,40 +68,32 @@ class ViewController: UITableViewController {
         }else {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
-        DispatchQueue.global(qos: .userInitiated).async{
-            if let url = URL(string: urlString) {
-                if let data = try? Data(contentsOf: url) {
-                    self.parse(json: data)
-                    return
-                }
+        if let url = URL(string: urlString){
+            if let data = try? Data(contentsOf: url){
+                //we're OK to parse!
+                parse(json: data)
+            } else {
+                showError()
             }
-            self.showError()
+        }
+        else {
+            showError()
+        }
     }
-}
-
     func parse(json: Data){
         let decoder = JSONDecoder()
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
-            DispatchQueue.main.async {
-                self.petitions = jsonPetitions.results
-                self.filteredPetitions = self.petitions  // 初始显示全部
-                self.tableView.reloadData()
-            }
-
+            petitions = jsonPetitions.results
+            filteredPetitions = petitions  // 初始显示全部
+            tableView.reloadData()
         }
     }
     
-    func showError() {
-        DispatchQueue.main.async {
-            let ac = UIAlertController(
-                title: "Loading error",
-                message: "There was a problem loading the feed; please check your connection and try again.",
-                preferredStyle: .alert
-            )
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            self.present(ac, animated: true)
-        }
+    func showError(){
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -118,7 +110,7 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = filteredPetitions[indexPath.row]
+        vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated:true)
     }
 
